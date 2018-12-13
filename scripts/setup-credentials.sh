39bd6631a5ab5a035389c6b935a01cbcefa1b7fd
julianedwards <julian.edwards@mongodb.com>
2018-12-13 13:27:45 -0500

    use subprocess.exec in evergreen.yml

---
+++ scripts/setup-credentials.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o errexit
+
+echo "building aws creds file!"
+
+mkdir ~/.aws
+cat <<EOF > ~/.aws/config
+[default]
+region = us-east-1
+EOF
+cat <<EOF > ~/.aws/credentials
+[default]
+aws_access_key_id = "$AWS_KEY"
+aws_secret_access_key = "$AWS_SECRET"
+EOF
