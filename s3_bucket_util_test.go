evergreen-ci/pail

commit 10686cd7fb4e407b49a2099d769331646b6fa372

DEVPROD-16582 Use s3manager.Upload for s3Bucket.Put (#129)
This commit adjusts an s3Bucket's .Put method to use the aws SDK's
UploadManager API. This API transparently uploads files to S3 concurrently.
In my testing, this commit noticably improves upload speeds for larger files.
For example, on rhel90-small a 1GB upload dropped from 24.5 seconds to 9.7
seconds. The current implementation was OOM killed on a 16GB upload because
we buffer the entire file contents in memory before uploading. The new
implementation finished the upload in 180 seconds while using only minimal
memory. So, in addition to greatly improving performance, we are also enabling
large puts to run on smaller instance sizes.

One notable difference in how I have chosen to implement things here is
by using the existing .Put methods instead of implementing a new method.
I did this because we didn't really need a new API to efficiently use the
UploadManager, and I wanted to take advantage of all of the existing tests
to gain confidence in the implementation.

I have used the existing GetToWriter concurrency algorithm for tuning this
parameter because in my testing it seemed to have the same performance
curve as GetToWriter did.

One thing you'll notice is that we are still buffering the entire compressed
file contents in memory if Compress: true is set. I could not come up with
a satisfactory way of writing to disk in a robust manner for Evergreen's
use here while still allowing this feature. Even though s3.put seems to
never use it, I wanted to keep things compatible to minimize changes in
Evergreen. In the future we could look at either removing this feature or
changing other aspects of how it works to keep s3.put's uploads consistently
streamed from disk.

s3_bucket.go
@@ -2,6 +2,7 @@ package pail
 
 import (
 	"archive/tar"
+	"bytes"
 	"compress/gzip"
 	"context"
 	"io"
@@ -754,17 +755,53 @@ func (s *s3Bucket) Reader(ctx context.Context, key string) (io.ReadCloser, error
 	return result.Body, nil
 }
 
-func putHelper(ctx context.Context, b Bucket, key string, r io.Reader) error {
-	f, err := b.Writer(ctx, key)
-	if err != nil {
-		return errors.WithStack(err)
+func putHelper(ctx context.Context, b *s3Bucket, key string, r io.Reader) error {
+	if b.dryRun {
+		return nil
 	}
-	_, err = io.Copy(f, r)
-	if err != nil {
-		_ = f.Close()
-		return errors.Wrap(err, "copying data to file")
+
+	if b.compress {
+		var buf bytes.Buffer
+
+		w := gzip.NewWriter(&buf)
+
+		if _, err := io.Copy(w, r); err != nil {
+			return errors.Wrap(err, "gzipping file")
+		}
+
+		r = bytes.NewReader(buf.Bytes())
 	}
-	return errors.WithStack(f.Close())
+
+	uploader := s3Manager.NewUploader(b.svc)
+	uploader.Concurrency = getManagerConcurrency()
+
+	key = b.normalizeKey(key)
+
+	input := &s3.PutObjectInput{
+		Body:   s3Manager.ReadSeekCloser(r),
+		Bucket: aws.String(b.name),
+		Key:    aws.String(key),
+		ACL:    s3Types.ObjectCannedACL(string(b.permissions)),
+	}
+
+	if b.contentType != "" {
+		input.ContentType = aws.String(b.contentType)
+	}
+
+	if b.ifNotExists {
+		input.IfNoneMatch = aws.String("*")
+	}
+
+	if b.compress {
+		input.ContentEncoding = aws.String(compressionEncoding)
+	}
+
+	if _, err := uploader.Upload(ctx, input); err != nil {
+		return errors.Wrapf(err, "uploading file")
+	}
+
+	return nil
+
 }
 
 func (s *s3BucketSmall) Put(ctx context.Context, key string, r io.Reader) error {
@@ -777,7 +814,7 @@ func (s *s3BucketSmall) Put(ctx context.Context, key string, r io.Reader) error 
 		"key":           key,
 	})
 
-	return putHelper(ctx, s, key, r)
+	return putHelper(ctx, &s.s3Bucket, key, r)
 }
 
 func (s *s3BucketLarge) Put(ctx context.Context, key string, r io.Reader) error {
@@ -790,7 +827,7 @@ func (s *s3BucketLarge) Put(ctx context.Context, key string, r io.Reader) error 
 		"key":           key,
 	})
 
-	return putHelper(ctx, s, key, r)
+	return putHelper(ctx, &s.s3Bucket, key, r)
 }
 
 func (s *s3Bucket) Get(ctx context.Context, key string) (io.ReadCloser, error) {
@@ -805,19 +842,22 @@ func (s *s3Bucket) Get(ctx context.Context, key string) (io.ReadCloser, error) {
 	return s.Reader(ctx, key)
 }
 
+func getManagerConcurrency() int {
+	// After quite a bit of testing, a minimum of 10 seems to perform the best,
+	// even on distros with fewer than 10 cores. 10 is also what the AWS cli
+	// defaults to. See DEVPROD-16611 for more information on this testing.
+	const minConcurrency = 10
+
+	return max(runtime.NumCPU(), minConcurrency)
+}
+
 // GetToWriter fetches the key from this bucket and writes the contents to
 // an io.WriterAt in parallel. This function uses the s3Manager.Downloader
 // API to download and write to this writer in parallel using byte ranges. This
 // method is significantly more efficient at fetching large files than Get.
 func (s *s3Bucket) GetToWriter(ctx context.Context, key string, w io.WriterAt) error {
 	downloader := s3Manager.NewDownloader(s.svc)
-
-	// After quite a bit of testing, a minimum of 10 seems to perform the best,
-	// even on distros with fewer than 10 cores. 10 is also what the AWS cli
-	// defaults to. See DEVPROD-16611 for more information on this testing.
-	const minConcurrency = 10
-
-	downloader.Concurrency = max(runtime.NumCPU(), minConcurrency)
+	downloader.Concurrency = getManagerConcurrency()
 
 	input := &s3.GetObjectInput{
 		Bucket: aws.String(s.name),

commit 06219446d4ea7fc5e1fd43051260b3c1d33d89f1

DEVPROD-17133 Close gzip buffer on compressed PutObject calls (#131)
This commit fixes a bug when Compress: true was passed to pail where we
forgot to close the gzip writer. This bug was introduced in 10686cd7fb4e407b49a2099d769331646b6fa372.
The tests did not catch this mistake because we were only testing the write
path through .Writer instead of .Put. This commit also adds a test for Put
which uses compression. Without the corresponding change to s3_bucket.go,
this test case will fail as expected.

s3_bucket.go
@@ -769,6 +769,10 @@ func putHelper(ctx context.Context, b *s3Bucket, key string, r io.Reader) error 
 			return errors.Wrap(err, "gzipping file")
 		}
 
+		if err := w.Close(); err != nil {
+			return errors.Wrap(err, "closing gzip writer")
+		}
+
 		r = bytes.NewReader(buf.Bytes())
 	}
 

s3_bucket_util_test.go
@@ -7,11 +7,13 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	s3Manager "github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	s3Types "github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/evergreen-ci/pail/testutil"
@@ -338,6 +340,45 @@ func getS3SmallBucketTests(ctx context.Context, tempdir string, s3Credentials aw
 				require.NoError(t, err)
 				require.NoError(t, r.Close())
 				assert.Equal(t, data, s3UncompressedData)
+			},
+		},
+		{
+			id: "TestCompressingPut",
+			test: func(t *testing.T, b Bucket) {
+				rawBucket := b.(*s3BucketSmall)
+				s3Options := S3Options{
+					Credentials: s3Credentials,
+					Region:      s3Region,
+					Name:        s3BucketName,
+					Prefix:      rawBucket.prefix,
+					MaxRetries:  aws.Int(20),
+					Compress:    true,
+				}
+				client := &http.Client{}
+				cb, err := NewFastGetS3BucketWithHTTPClient(ctx, client, s3Options)
+				require.NoError(t, err)
+
+				data := []byte{}
+				for i := 0; i < 300; i++ {
+					data = append(data, []byte(testutil.NewUUID())...)
+				}
+
+				compressedKey := testutil.NewUUID()
+				require.NoError(t, cb.Put(ctx, compressedKey, bytes.NewReader(data)))
+
+				buf := s3Manager.NewWriteAtBuffer([]byte{})
+				require.NoError(t, cb.GetToWriter(ctx, compressedKey, buf))
+
+				s3CompressedData, err := io.ReadAll(bytes.NewReader(buf.Bytes()))
+				require.NoError(t, err)
+
+				gzr, err := gzip.NewReader(bytes.NewReader(s3CompressedData))
+				require.NoError(t, err)
+				gotData, err := io.ReadAll(gzr)
+				require.NoError(t, err)
+				require.NoError(t, gzr.Close())
+
+				assert.Equal(t, data, gotData)
 			},
 		},
 		{